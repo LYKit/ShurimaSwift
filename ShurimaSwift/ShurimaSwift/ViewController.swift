d5f4ff4d59012e6d92312f7a8f2edca7305e5b61
JVSugar <471043396@qq.com>
2018-03-16 11:22:13 +0800

    init project

---
+++ ShurimaSwift/ShurimaSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShurimaSwift
+//
+//  Created by jinghua on 2018/3/16.
+//  Copyright © 2018年 ME.Sugar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("init project")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
