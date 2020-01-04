a585ee1fab436ffe6ce002d56e825cfc035a3768
WJSugar <471043396@qq.com>
2020-01-04 13:06:31 +0800

    test

--- ShurimaSwift/class/home/SRMHomeController.swift
+++ ShurimaSwift/class/home/SRMHomeController.swift
@@ -8,6 +8,9 @@
 
 import UIKit
 import YYKit
+import SnapKit
+import Moya
+import Alamofire
 
 class SRMHomeController: SRMBaseViewController {
 
@@ -16,8 +19,16 @@ class SRMHomeController: SRMBaseViewController {
         title = "首页"
         view.backgroundColor = .yellow
         
+        let redView = UIView()
+        view.addSubview(redView)
+        redView.snp.makeConstraints { (make) in
+            make.top.equalTo(0);
+            make.left.equalTo(0);
+            make.width.equalTo(SCREEN_WIDTH);
+            make.height.equalTo(400);
+        }
+        redView.backgroundColor = UIColor.red
         
-
     }
 
     override func didReceiveMemoryWarning() {
