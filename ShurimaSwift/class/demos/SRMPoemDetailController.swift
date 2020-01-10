e1ff5c5f8e0f0e2a9bae788dafeb5b8c5f43a23a
Sugar <471043396@qq.com>
2020-01-10 19:19:27 +0800

    model json convert

--- ShurimaSwift/class/demos/SRMPoemDetailController.swift
+++ ShurimaSwift/class/demos/SRMPoemDetailController.swift
@@ -8,6 +8,9 @@
 
 import UIKit
 import Moya
+import SwiftyJSON
+import Alamofire
+import AlamofireObjectMapper
 
 
 enum MoyaApi {
@@ -25,14 +28,33 @@ class SRMPoemDetailController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .white
         
-//        https://v2.jinrishici.com/one.json
-        let provider = MoyaProvider<MoyaApi>()
-        provider.request(MoyaApi.detail) { result in
-            if case let .success(response) = result {
-                let mapJson = try? response.mapJSON()
-                print(mapJson)
-            }
+        let url = "https://v2.jinrishici.com/one.json"
+        
+        
+        Alamofire.request(url).responseObject { (response: DataResponse<SRMPoem>) in
+            let poem = response.result.value;
+            print(poem?.cacheAt)
         }
+        
+        
+        
+        
+        
+        
+////        https://v2.jinrishici.com/one.json
+//        let provider = MoyaProvider<MoyaApi>()
+//        provider.request(MoyaApi.detail) { result in
+//            if case let .success(response) = result {
+////                let json = JSON(response.data)
+////                print(json)
+//                do {
+//                    let test = try JSONDecoder().decode(SRMPoem.self, from: response.data);
+//                    print(test.cacheAt)
+//                } catch let error {
+//
+//                }
+//            }
+//        }
     }
 }
 
